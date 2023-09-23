194ead5228442cd8c27e5ab859ae347d8a2da615
sonah5009 <sonah5009@gmail.com>
2023-09-24 03:37:31 +0900

    img center

--- javascript/index.js
+++
@@ -1,9 +0,0 @@
-const menuHeight = document.querySelector("#main-header").offsetHeight;
-
-const moveLocation = document.querySelector("#move").offsetTop;
-
-console.log(menuHeight, moveLocation)
-
-// window.scrollTo({top:moveLocation - menuHeight, behavior:'smooth'});
-
-window.scroll({ top: menuHeight, left: 0, behavior: 'smooth' });
